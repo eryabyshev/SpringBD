925ef908082e89328be9a6faa6462499ce0525cc
Evgeny Ryabyshew <34579787+evgeny-rybishew@users.noreply.github.com>
2018-02-27 19:06:17 +0300

    error while selecting bd

--- MP3DB.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `MP3` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `author` varchar(32) COLLATE utf8_bin NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
