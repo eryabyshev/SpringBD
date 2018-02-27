4a3305c905ad76c1223b16e8fe677cb10315eb43
Evgeny Ryabyshew <34579787+evgeny-rybishew@users.noreply.github.com>
2018-02-27 20:13:08 +0300

    implement insert into db

--- MP3Dao.java
+++ MP3Dao.java
@@ -1,5 +1,18 @@
 package ru.evgeny.dao.interfaces;
 
-public interface MP3Dao {
+import java.util.List;
 
+import ru.evgeny.dao.obj.MP3;
+
+public interface MP3Dao {
+	void insert(MP3 mp3);
+	
+	void delete(MP3 mp3);
+	
+	MP3 getMP3byID(int id);
+	
+	List<MP3> getMP3ListByName(String name);
+	
+	List<MP3> getMP3ListByAuthor(String author);
+		
 }
